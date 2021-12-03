ba4d712cf8307e193f723fc4241e73f02467a4b1
Kayla Sween <kaylasween@Kaylas-MBP.sween.network>
2021-12-02 19:13:42 -0600

    #5 removing blitz eslint rules and adding some other ones.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-  extends: ["blitz"],
+  parser: "babel-eslint",
+  rules: {
+    //arrow functions
+    "no-confusing-arrow": "error",
+
+    //console statements
+    "no-console": "error",
+
+    //curly braces
+    "brace-style": "error",
+    curly: "error",
+    "object-curly-spacing": ["error", "always"],
+
+    //spaces
+    "no-trailing-spaces": "error",
+
+    //variables
+    "no-var": "error",
+  },
 }
