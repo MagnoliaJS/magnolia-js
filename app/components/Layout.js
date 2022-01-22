911421f3f6a26040a6c10fb1dc1b04c18ac52c3b
Kayla Sween <kaylasween@Kaylas-MBP.sween.network>
2022-01-22 14:20:50 -0600

    adding twitter link

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,5 +1,6 @@
 import { Image, Link } from "blitz"
 import logo from "public/MagnoliaHorizontalLogo-DarkJS.png"
+import twitter from "public/icons/twitter.svg"
 
 const Layout = (props) => (
   <div className="flex flex-col bg-pink-100 p-3 min-h-screen">
@@ -20,10 +21,17 @@ const Layout = (props) => (
         </Link>
       </div>
     </nav>
-    <main className="flex-grow md:mx-10 lg:mx-48 my-8 px-3 md:px-4 py-4 bg-white">
+    <main className="flex-grow md:mx-10 lg:mx-48 mt-8 mb-4 px-3 md:px-4 py-4 bg-white">
       {props.children}
     </main>
-    <footer>&copy; MagnoliaJS</footer>
+    <footer className="flex justify-between md:mx-10 lg:mx-48 px-3 md:px-4">
+      <div>
+        <a href="https://twitter.com/magnoliajsconf">
+          <Image src={twitter} alt="link to Twitter account." height="16" width="16" />
+        </a>
+      </div>
+      <div>&copy; MagnoliaJS</div>
+    </footer>
   </div>
 )
 
