3e3480a25232fee9f55a0c3f398c97be233e397f
Kayla Sween <kaylasween@Kaylas-MBP.sween.network>
2022-07-01 19:39:08 -0500

    switching to netlify form.

--- app/pages/sponsors/sponsorship-info.js
+++ app/pages/sponsors/sponsorship-info.js
@@ -42,7 +42,8 @@ const SponsorshipInfo = () => {
         you ASAP.
       </p>
 
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form} data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="sponsorship-interest" />
         <label htmlFor="name" className={styles.label}>
           Name
         </label>
