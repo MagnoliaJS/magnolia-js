21e7209213671104d07dc349942fe761e77de243
Richard Sween <sweenr418@gmail.com>
2022-08-04 15:44:41 -0500

    Add speaker page inital pass

---
+++ app/components/Speaker.js
@@ -0,0 +1,26 @@
+import { Image } from "blitz"
+
+import styles from "./Speaker.module.css"
+
+const Speaker = (props) => {
+  return (
+    <div className={styles.speaker}>
+      {props.photo ? (
+        <Image src={props.photo} alt={props.name} layout="responsive" height="100%" width="100%" />
+      ) : null}
+      <div className={styles.info}>
+        <h2 className={styles.name}>{props.name}</h2>
+        <a
+          href={`https://twitter.com/${props.twitter.substring(1)}`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          {props.twitter}
+        </a>
+        <p>{props.title ? props.title : "Talk info coming soon!"}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Speaker
