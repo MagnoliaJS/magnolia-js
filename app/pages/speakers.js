5d427f6379b85b52e07585dfc2709b125c3c9ae9
Kayla Sween <kaylasween@kaylas-mbp.attlocal.net>
2022-09-19 11:57:31 -0500

    removing speakers.

--- app/pages/speakers.js
+++ app/pages/speakers.js
@@ -1,24 +1,24 @@
-import { Head } from "blitz"
-import Speaker from "../components/Speaker"
+// import { Head } from "blitz"
+// import Speaker from "../components/Speaker"
 // import speakers from "public/speakers/speakers.json"
 
-const Speakers = () => (
-  <div className="center-text">
-    <Head>
-      <title>Speakers | MagnoliaJS</title>
-    </Head>
-    <h1>Speakers</h1>
-    <p>Come learn from such incredibly talented people as:</p>
+// const Speakers = () => (
+//   <div className="center-text">
+//     <Head>
+//       <title>Speakers | MagnoliaJS</title>
+//     </Head>
+//     <h1>Speakers</h1>
+//     <p>Come learn from such incredibly talented people as:</p>
 
-    <div>
-      {speakers
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map((speaker) => {
-          return <Speaker {...speaker} />
-        })}
-    </div>
-  </div>
-)
+//     <div>
+//       {speakers
+//         .sort((a, b) => a.name.localeCompare(b.name))
+//         .map((speaker) => {
+//           return <Speaker {...speaker} />
+//         })}
+//     </div>
+//   </div>
+// )
 
-Speakers.suppressFirstRenderFlicker = true
-export default Speakers
+// Speakers.suppressFirstRenderFlicker = true
+// export default Speakers
