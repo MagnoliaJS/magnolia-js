65e1c93e8a5890d2c2bcd4ca4654ff798cd7740c
Kayla Sween <kaylasween@Kaylas-MacBook-Pro.local>
2025-06-19 13:01:24 -0500

    initial 2025 commit.

--- schemas/index.js
+++
@@ -1,8 +0,0 @@
-import recommendation from "./recommendation"
-import recommendationSubType from "./recommendationSubType"
-import recommendationType from "./recommendationType"
-import speaker from "./speaker"
-import sponsor from "./sponsor"
-import sponsorshipTier from "./sponsorshipTier"
-
-export const schemaTypes = [recommendation, recommendationSubType, recommendationType, speaker, sponsor, sponsorshipTier]
